c2ce004e807ba8560a9114c5e44e940a3e8b3e8a
KavitaJadhav <jadhavkavita206@gmail.com>
2023-05-19 09:36:37 +0530

    Added an example of switch with enum.

--- src/Switch.java
+++ src/Switch.java
@@ -1,39 +1,40 @@
+enum Cars {
+    BMW,
+    JEEP,
+    AUDI,
+    VOLKSWAGEN,
+    NANO,
+    FIAT;
+}
+
 public class Switch {
+    public static void main(String args[]) {
+        Cars car;
+        car = Cars.AUDI;
 
-    public static void main(String[] args) {
-        String value = "bubble";
-        switch (value) {
-            case "bubble":
-                System.out.println("input type bubble sort");
+        switch (car) {
+
+            case BMW:
+                System.out.println("You choose BMW !");
                 break;
-            case "merge":
-                System.out.println("input type merge sort");
+            case JEEP:
+                System.out.println("You choose JEEP !");
                 break;
-            case "quick":
-                System.out.println("input type quick sort");
+            case AUDI:
+                System.out.println("You choose AUDI !");
                 break;
+            case VOLKSWAGEN:
+                System.out.println("You choose VOLKSWAGEN !");
+                break;
+            case NANO:
+                System.out.println("You choose NANO !");
+                break;
+            case FIAT:
+                System.out.println("You choose FIAT !");
+
             default:
-                System.out.println("invalid option");
+                System.out.println("NEW BRAND'S CAR.");
+                break;
         }
-
-        int num = 0;
-        switch (num) {
-            case 0:
-                System.out.println("input is 0");
-                break;
-            case 1:
-                System.out.println("input is 1");
-                break;
-            case 2:
-                System.out.println("input is 2");
-                break;
-            default:
-                System.out.println("invalid option");
-        }
-    }
-
-
-    public String hello(String name){
-        return "hello" + name;
     }
 }
